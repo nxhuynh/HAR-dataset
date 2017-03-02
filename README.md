2f888216d1e71129b56c5498fac9a07e49bcd814
Tom Huynh <nghia.huynh@utexas.edu>
2017-03-02 17:41:38 -0600

    add the dataset, script, and code book

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# HAR-dataset
-Processed data from UCI HAR dataset
+# Processed HAR Dataset
+This repo contains a version of the UCI HAR dataset as followed:
+1.	Merges the training and the test sets to create one data set.
+2.	Extracts only the measurements on the mean and standard deviation for each 
+	measurement.
+3.	Uses descriptive activity names to name the activities in the data set
+4.	Appropriately labels the data set with descriptive variable names.
+5.	From the data set in step 4, creates a second, independent tidy data set 
+	with the average of each variable for each activity and each subject.
+
+### Files included:
+* Dataset1.csv - result of step 1-4
+* Dataset2.csv - result of step 5
+* features_info.txt - explanation of the features
+* features.txt - list of features
+* README.md 
+* run_analysis.R - the script to process the HAR dataset
+
+
+More info about the original UCI HAR dataset can be found here:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+library(data.table)
+library(dplyr)
+
+# 1. Merges the training and the test sets to create one data set.
+x_train <- fread("UCI HAR Dataset/train/X_train.txt", colClasses = rep("numeric",561))
+x_test <- fread("UCI HAR Dataset/test/X_test.txt", colClasses = rep("numeric",561))
+x <- rbindlist(list(x_train, x_test))
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+f <- fread("UCI HAR Dataset/features.txt")
+f1 <- grep("mean()", f$V2)
+f2 <- grep("std()", f$V2)
+dataset <- x[,sort(c(f1,f2)), with=FALSE]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+y_train <- fread("UCI HAR Dataset/train/y_train.txt", colClasses = c("numeric"))
+y_test <- fread("UCI HAR Dataset/test/y_test.txt", colClasses = c("numeric"))
+activities <- fread("UCI HAR Dataset/activity_labels.txt")
+y <- rbindlist(list(y_train, y_test))
+labels <- factor(factor(y$V1), labels=activities$V2)
+
+# 4. Appropriately labels the data set with descriptive variable names.
+vnames <- f$V2[c(f1,f2)] %>% 
+    gsub("^t", "Time Domain ", .) %>%
+    gsub("^f", "Frequency Domain ", .) %>% 
+    gsub("Acc"," Accelerometer", .) %>%
+    gsub("Gyro"," Gyroscope", .) %>% 
+    gsub("Jerk"," Jerk", .) %>% 
+    gsub("Mag"," Magnitude", .) %>% 
+    gsub("-mean\\(\\)", " Mean", .) %>%
+    gsub("-meanFreq\\(\\)", " Mean", .) %>%
+    gsub("-std\\(\\)", " Standard Deviation", .) %>% 
+    gsub("-([X-Z])"," \\1", .)
+
+colnames(dataset) <- vnames
+
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+#    with the average of each variable for each activity and each subject.
+subjects_train <- fread("UCI HAR Dataset/train/subject_train.txt")
+subjects_test <- fread("UCI HAR Dataset/test/subject_test.txt")
+subjects <- rbindlist(list(subjects_train, subjects_test))$V1
+features <- names(dataset)
+dataset$Subject <- subjects
+dataset$Activity <- labels
+ds2 <- expand.grid(unique(subjects), unique(labels))
+names(ds2) <- c("Subject", "Activity")
+for (j in 1:length(features)){
+    col_buf = numeric(nrow(ds2))
+    for (i in 1:nrow(ds2)){
+        row = (dataset$Subject == ds2[i,1]) & (dataset$Activity == ds2[i,2])
+        datapt = colMeans(dataset[row, j, with=FALSE])
+        col_buf[i] <- datapt
+    }
+    ds2[[features[j]]] <- col_buf
+}
+
+write.csv(dataset, "Dataset1.csv", row.names=FALSE)
+write.csv(ds2, "Dataset2.csv", row.names=FALSE)
